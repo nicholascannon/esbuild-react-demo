5ebe6d7d8aacc822302ef0e84c6f7ee3c60af6b7
Nicholas Cannon <nicholascannon1@gmail.com>
2022-07-09 15:09:58 +0800

    small cleanups

--- src/app.tsx
+++ src/app.tsx
@@ -1,18 +1,25 @@
 import React, { useCallback, useState } from 'react';
 
-function App({ message }: { message: string }): JSX.Element {
-  const [count, setCount] = useState(0);
-  const increment = useCallback(() => {
-    setCount((currentCount) => currentCount + 1);
-  }, [count]);
+type PropTypes = {
+    message: string;
+};
 
-  return (
-    <>
-      <h1>{message}</h1>
-      <h2>Count: {count}</h2>
-      <button type="button" onClick={increment}>Increment</button>
-    </>
-  );
+const App = ({ message }: PropTypes): JSX.Element => {
+    const [count, setCount] = useState(0);
+    const increment = useCallback(() => {
+        setCount((currentCount) => currentCount + 1);
+    }, [count]);
+
+    return (
+        <>
+            <h1>Poop</h1>
+            <h1>{message}</h1>
+            <h2>Count: {count}</h2>
+            <button type='button' onClick={increment}>
+                Increment
+            </button>
+        </>
+    );
 };
 
 export default App;
